34b94a0223acc0f6fca4fea88f84cbaeabda7da8
Esteban Ordonez <estebanusco@gmail.com>
2021-11-08 15:18:20 -0800

    Tier II completed

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -3,8 +3,7 @@ import { ItemsContext } from './App';
 import { NavLink } from 'react-router-dom';
 
 export default function ItemCard(props) {
-
-  const { id, name, variants } = props;
+  const { id, name, variants, imageUrls } = props;
   const { handleItemSelect } = useContext(ItemsContext);
 
   let extraItems = 0;
@@ -13,35 +12,42 @@ export default function ItemCard(props) {
   return (
     <div className="outer__card__container">
       <div className="card">
-        <div className="card__container">
-          <div className="card__header">
-            <h3 className="card__title">{name}</h3>
+        {imageUrls.length > 0 && (
+          <img className="card__image" src={imageUrls[0].url} />
+        )}
+        <div className="card__overlay">
+          <div className="card__container">
+            <div className="card__header">
+              <h3 className="card__title">{name}</h3>
+            </div>
+            <div className="card__content">
+              {variants.map((variant, index) => {
+                if (index > 3) {
+                  addExtra();
+                  return null;
+                }
+                return (
+                  <p key={variant.id}>
+                    <span className="item">Item</span> {index + 1}{' '}
+                    {variant.name}
+                  </p>
+                );
+              })}
+              {extraItems ? <p>{`...plus ${extraItems} more`}</p> : null}
+            </div>
           </div>
-          <div className="card__content">
-            {variants.map((variant, index) => {
-              if (index > 3) {
-                addExtra();
-                return null;
-              }
-              return (
-                <p key={variant.id}>
-                  <span className="item">Item</span> {index + 1} {variant.name}
-                </p>
-              );
-            })}
-            {extraItems ? <p>{`...plus ${extraItems} more`}</p> : null}
+          <div className="items-list__select-btn-container">
+            <NavLink
+              to={`/${id}`}
+              className="btn btn--succes"
+              onClick={() => handleItemSelect(id)}
+            >
+              select
+            </NavLink>
           </div>
-        </div>
-        <div className="items-list__select-btn-container">
-          <NavLink
-            to={`/${id}`}
-            className="btn btn--succes"
-            onClick={() => handleItemSelect(id)}
-          >
-            select
-          </NavLink>
         </div>
       </div>
+
       <h3 className="card__legend">{name}</h3>
     </div>
   );
